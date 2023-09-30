9efef6511dc4e4a72dae64aaba25b9cf2a483134
rhanielfarias <rhanielfarias@gmail.com>
2023-09-30 17:57:13 -0300

    Criando a opção do usuário criar o seu tópico e a quantidade de mensagens que deseja transmitir

--- mqtt.c
+++ mqtt.c
@@ -3,18 +3,20 @@
 #include <string.h>
 #include "MQTTAsync.h"
 #include "MQTTClient.h"
-
+#include <locale.h>
 #define ADDRESS     "tcp://localhost:1883"
 #define CLIENTID    "ExemploClienteC"
-#define TOPIC       "meu_topico"
-#define PAYLOAD     "Mensagem de teste"
 #define QOS         1
 
 int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
     MQTTClient client;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
     MQTTClient_message pubmsg = MQTTClient_message_initializer;
     MQTTClient_deliveryToken token;
+
+    char user_input[1000];
+    char topic[500];
 
     MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
 
@@ -26,14 +28,26 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = strlen(PAYLOAD);
-    pubmsg.qos = QOS;
-    pubmsg.retained = 0;
+    printf("Digite o topico MQTT que deseja criar, por favor: ");
+    scanf("%500s", topic);
 
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+    int num_messages;
+    printf("Quantas mensagens deseja enviar para o topico %s? ", topic);
+    scanf("%d", &num_messages);
 
-    MQTTClient_waitForCompletion(client, token, 10000);
+    for (int i = 0; i < num_messages; i++) {
+        printf("Digite a mensagem %d: ", i + 1);
+        scanf(" %[^\n]s", user_input);
+
+        pubmsg.payload = user_input;
+        pubmsg.payloadlen = strlen(user_input);
+        pubmsg.qos = QOS;
+        pubmsg.retained = 0;
+
+        MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+
+        MQTTClient_waitForCompletion(client, token, 10000);
+    }
 
     MQTTClient_disconnect(client, 10000);
 
